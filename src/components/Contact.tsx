88e5762e325f87425b2b7f52606e0b1c6b10c210
Anderson Mattar <andmat7745@gmail.com>
2023-09-19 16:05:59 -0300

    deploy

---
+++ src/components/Contact.tsx
@@ -0,0 +1,173 @@
+import emailjs from "@emailjs/browser";
+import { FormEvent, useRef, useState } from "react";
+import { FaSpinner, FaWhatsapp } from "react-icons/fa";
+import {
+  HiCheckCircle,
+  HiOutlineEnvelope,
+  HiOutlineMapPin,
+} from "react-icons/hi2";
+
+export function Contact() {
+   const form = useRef<HTMLFormElement>(null)
+   const [loading, setLoading] = useState(false)
+   const [success, setSuccess] = useState(false)
+   const [error, setError] = useState(false)
+
+   const sendEmail = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    if(!form.current) return
+
+    setLoading(true)
+
+    emailjs
+    .sendForm(
+    "service_hyw1vxq",
+    "template_ffzwchq",
+    form.current,
+    "ZbNkzLi_1qXP2zzt1"
+    )
+    .then(
+    () => {
+      setSuccess(true)
+      setLoading(false)
+    },
+    (error) => {
+      setError(true)
+      setLoading(false)
+      console.error(error)
+    }
+    )
+   }
+
+  const contacts = [
+    {
+      name: "WhatsApp",
+      description: "+55 32 98463-8048",
+      link: "https://web.whatsapp.com/?text=Olá...",
+      icon: <FaWhatsapp className="h-10 w-10" />,
+    },
+    {
+      name: "Email",
+      description: "andmat7745@gmail.com",
+      link: "mailto:andmat7745@gmail.com?subject=Olá...",
+      icon: <HiOutlineEnvelope className="h-10 w-10" />,
+    },
+    {
+      name: "São João Del Rei",
+      description: "Minas Gerais",
+      link: "https://www.google.com.br/maps/place/S%C3%A3o+Jo%C3%A3o+del+Rei,+MG/@-21.1615364,-44.3792588,11z/data=!3m1!4b1!4m6!3m5!1s0xa1c6613400d4bf:0xb5d1a2098496fa8f!8m2!3d-21.1539436!4d-44.2289441!16s%2Fg%2F11rgdh3swk?entry=ttu",
+      icon: <HiOutlineMapPin className="h-10 w-10" />,
+    },
+  ];
+  return (
+    <section id="contact" className="bg-blue-700 text-white">
+      <div className="container mx-auto max-w-4xl p-4 py-8 ">
+        <div className="mb-6">
+          <h2 className="z-50 mb-2">
+            <span className="mr-2 font-headline text-3xl font-semibold">
+              Fale
+            </span>
+            <span className="font-handwriting text-4xl">Comigo</span>
+            <p className="text-sm">
+              Entre em contato pelo formulário ou WhatsApp, com certeza poderei
+              te ajudar.
+            </p>
+          </h2>
+        </div>
+        <div className="flex flex-col gap-6 md:flex-row">
+          <div className="basis-2/3">
+            <form ref={form} onSubmit={sendEmail}>
+              <div className="mb-4">
+                <label
+                  htmlFor="message"
+                  className="mb-2 block ps-4 font-headline font-semibold"
+                >
+                  Mensagem:
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  className="h-36 w-full rounded-lg border border-white bg-transparent p-2 outline-none"
+                  required
+                />
+              </div>
+
+              <div className="mb-6 flex flex-col gap-4 md:flex-row">
+                <div className="flex-grow">
+                  <label
+                    htmlFor="fullName"
+                    className="mb-2 block ps-4 font-headline font-semibold"
+                  >
+                    Seu nome:
+                  </label>
+                  <input
+                    className="w-full rounded-lg border border-white bg-transparent p-2 outline-none"
+                    type="text"
+                    name="fullName"
+                    id="fullName"
+                    required
+                  />
+                </div>
+                <div className="flex-grow">
+                  <label
+                    htmlFor="email"
+                    className="mb-2 block ps-4 font-headline font-semibold"
+                  >
+                    Seu email:
+                  </label>
+                  <input
+                    className="w-full rounded-lg border border-white bg-transparent p-2 outline-none"
+                    type="email"
+                    name="email"
+                    id="email"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div>
+              <button
+                  type="submit"
+                  className="button flex items-center gap-2 text-blue-700"
+                  disabled={loading}
+                >
+                  {loading && <FaSpinner className="h-4 w-4 animate-spin" />}
+                  {success && <HiCheckCircle className="h-4 w-4" />}
+                  Enviar mensagem
+                </button>
+                
+                {error && (
+                  <p className="mt-2">
+                    Ocorreu um erro ao enviar a mensagem, tente novamente mais
+                    tarde.
+                  </p>
+                )}
+              </div>
+            </form>
+          </div>
+          <div className="basis-1/3">
+            {contacts.map((contact, index) => (
+              <div
+                key={`contact-${index}`}
+                className="mb-4 flex items-center gap-4 rounded-lg border 
+                      border-dashed border-gray-400 p-4"
+              >
+                {contact.icon}
+                <p className="font-headline font-semibold">{contact.name}</p>
+                <a
+                  href={contact.link}
+                  target="_blank"
+                  title={`Link para ${contact.link}`}
+                  className="text-gray-300 underline underline-offset-2"
+                >
+                  {contact.description}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Projects.tsx
@@ -0,0 +1,72 @@
+import { HiArrowTopRightOnSquare } from "react-icons/hi2"
+
+export function Projects(){
+  const projects = [
+    {
+      title: "Site All Tech Solutions - Home",
+      description: "Projeto próprio criado com React.js",
+      image: "images/mockup1.png",
+      link: "https://alltechsolutions.netlify.app/",
+      colSpan: "col-span-1",
+    },
+    {
+      title: "Site All Tech Solutions - Sobre",
+      description: "Projeto criado com objetivo de praticar o desenvolvimento com react.js e para expor meu trabalho ",
+      image:"images/mockup2.png",
+      link: "https://alltechsolutions.netlify.app/",
+      colSpan: "col-span-1 md:col-span-1",
+    },
+    {
+      title: "Site All Tech Solutions - Contatos",
+      description: "Um projeto simples porém criado com muito esforçoe estudo",
+      image: "images/mockup3.png",
+      link: "https://alltechsolutions.netlify.app/",
+      colSpan: "col-span-1 md:col-span-1",
+    },
+  ]
+
+  return(
+    <>
+    <section className="rounded-tl-[80px] bg-gradient-to-tr from-black to-gray-900
+    text-white md:rounded-tl-[180px]">
+      <div className="container mx-auto max-w-4xlp-4 py-12">
+        <div className="relative p-4 text-center">
+          <h2 className="relative z-50 mb-2 text-white">
+            <span className="mr-2 font-headline text-3xl font-semibold">Projetos &</span>
+            <span className="font-handwriting text-5xl">Portfólio</span>
+          </h2>
+          <p className="relative text-sm text-gray-400">
+            Compartilho meu primeiro projeto de desenvolvimento web, um site simples, 
+            porém feito com paixão e dedicação, representando o início da minha jornada como desenvolvedor web!
+          </p>
+          <div className="absolute left-1/2 top-3 z-0 h-10 w-10 rounded-lg bg-blue-400/10" />
+        </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          {
+            projects.map((project, index) => (
+              <div 
+              className={`group relative h-52 cursor-default rounded-lg ${project.colSpan} bg-cover bg-center`} 
+              key={`project-${index}`}
+              style={{ backgroundImage: `url('${project.image}')` }}
+              >
+               <div className="absolute inset-0 flex h-full w-full flex-col
+               items-center justify-center rounded-lg bg-blue-600 text-white
+               opacity-0 transition-opacity group-hover:opacity-100">
+                  <h4 className="font-headline text-lg font-semibold">
+                    {project.title}
+                  </h4>
+                      <p className="mb-4 text-sm"> {project.description} </p>
+                      <a href={project.link} target="_blank" title={`Link para ${project.title}`}>
+                          <HiArrowTopRightOnSquare className="h-6 w-6" />
+                       </a>
+                       
+               </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </section>
+    </>
+  )
+}
