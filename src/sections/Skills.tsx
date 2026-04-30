16216496a4cbf35d9f226029d26654c7138a009d
unknown <andmat7745Ggmail.com>
2026-04-30 20:28:02 -0300

    Termino das sessões e responsividade geral

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -3,7 +3,10 @@ import {
   FaReact,
   FaNodeJs,
   FaPython,
-  FaGitAlt
+  FaGitAlt,
+  FaJava,
+  FaPhp,
+  FaJs,  
 } from "react-icons/fa";
 
 import {
@@ -11,19 +14,29 @@ import {
   SiMysql,
   SiSqlite,
   SiBootstrap,
-  SiVuedotjs
+  SiVuedotjs,
+  SiNextdotjs,
+  SiVite,
+  SiAngular,
+
 } from "react-icons/si";
 
 const skills = [
-  { name: "React", icon: <FaReact /> },
-  { name: "TypeScript", icon: <SiTypescript /> },
-  { name: "Node.js", icon: <FaNodeJs /> },
-  { name: "Python", icon: <FaPython /> },
-  { name: "Vue.js", icon: <SiVuedotjs /> },
-  { name: "MySQL", icon: <SiMysql /> },
-  { name: "SQLite", icon: <SiSqlite /> },
-  { name: "Bootstrap", icon: <SiBootstrap /> },
-  { name: "Git", icon: <FaGitAlt /> },
+  { name: "React", icon: <FaReact />, text: "Nível Intermediário." },
+  { name: "TypeScript", icon: <SiTypescript />, text: "Nível Intermediário." },
+  { name: "Node.js", icon: <FaNodeJs />, text: "Nível Intermediário." },
+  { name: "Python", icon: <FaPython />, text: "Nível Básico." },
+  { name: "Vue.js", icon: <SiVuedotjs />, text: "Nível Básico." },
+  { name: "MySQL", icon: <SiMysql />, text: "Nível Intermediário." },
+  { name: "SQLite", icon: <SiSqlite />, text: "Nível Intermediário." },
+  { name: "Bootstrap", icon: <SiBootstrap />, text: "Nível Intermediário." },
+  { name: "Git", icon: <FaGitAlt />, text: "Nível Intermediário." },
+  { name: "Java", icon: <FaJava />, text: "Nível Básico." },
+  { name: "PHP", icon: <FaPhp />, text: "Nível Básico." },
+  { name: "JavaScript", icon: <FaJs />, text: "Nível Intermediário." },
+  { name: "Next.js", icon: <SiNextdotjs />, text: "Nível Básico." },
+  { name: "Vite", icon: <SiVite />, text: "Nível Intermediário." },
+  { name: "Angular", icon: <SiAngular />, text: "Nível Básico." },
 ];
 
 export default function Skills() {
@@ -48,6 +61,7 @@ export default function Skills() {
           >
             <div className="icon">{skill.icon}</div>
             <span>{skill.name}</span>
+            <p>{skill.text}</p>
           </motion.div>
         ))}
       </div>
