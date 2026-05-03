8f4a9de5bb8f9f39ea9e8953fdfb17cc616b5f9f
unknown <andmat7745Ggmail.com>
2026-05-03 10:52:20 -0300

    acerto de texto da sessão projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,17 +7,17 @@ export interface Project {
 export const projects: Project[] = [
   {
     title: "Casa de Violeta",
-    desc: "Landing page com foco em conversão",
+    desc: "Landing page com foco em conversão, para pousada localizada em Tiradentes, Minas Gerais.",
     link: "https://www.casadevioletapousada.com.br/"
   },
   {
     title: "Fish Caiaques",
-    desc: "Site de ecoturismo",
+    desc: "Site de ecoturismo, para empresa de aluguel de caiaques em Bertioga, litoral sul de São Paulo.",
     link: "https://fishcaiaquesecoturismo.netlify.app/"
   },
   {
     title: "AllTech Solutions",
-    desc: "SPA institucional",
+    desc: "SPA institucional, a principio meu primeiro projeto acadêmico mas, agora um site de negocio real.",
     link: "https://alltechsolutions.netlify.app/"
   }
 ];
